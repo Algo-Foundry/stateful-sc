3a2ff4f89cc89efb19e5b398b408c714d1551def
Charlie Kong <charlie@undercurrent.tech>
2022-05-10 21:28:04 +0800

    solution

--- assets/game_approval.py
+++ assets/game_approval.py
@@ -1,9 +1,87 @@
+import random
 from pyteal import *
 
 def game():
-    handle_creation = Return(Int(1))
-    handle_optin = Return (Int(1))
-    handle_noop = Return(Int(1))
+    '''
+    Initialize monster with specified health
+    '''
+    monsterHealth = Btoi(Txn.application_args[0])
+    handle_creation = Seq([
+        Assert(monsterHealth >= Int(5)),
+        App.globalPut(Bytes("Health"), monsterHealth),
+        App.globalPut(Bytes("MaxDamage"), Int(0)),
+        Return(Int(1))
+    ])
+
+    '''
+    Initialize player's damage dealt to the monster
+    '''
+    handle_optin = Seq([
+        Assert(App.optedIn(Txn.sender(), Txn.application_id())),
+        App.localPut(Txn.sender(), Bytes("Damage"), Int(0)),
+        Return(Int(1))
+    ])
+
+    '''
+    Attacks the monster
+    '''
+    currentMonsterHealth = App.globalGet(Bytes("Health"))
+    playerDamage = Int(2) # deal 2 damage
+    playerCurrentDamage = App.localGet(Txn.sender(), Bytes("Damage")) # returns 0 if state is not found
+    playerTotalDamage = playerCurrentDamage + playerDamage
+    currentMvpDamage = App.globalGet(Bytes("MaxDamage")) # highest amount of damage dealt to monster    
+    
+    update_player_total_damage = Seq(
+        App.localPut(Txn.sender(), Bytes("Damage"), playerTotalDamage),
+    )
+
+    update_monster_health = If(
+        playerDamage > currentMonsterHealth, 
+        App.globalPut(Bytes("Health"), Int(0)), # monster is dead
+        App.globalPut(Bytes("Health"), currentMonsterHealth - playerDamage) # reduce monster health
+    )
+
+    update_mvp = If(
+        playerTotalDamage > currentMvpDamage,
+        Seq([
+            App.globalPut(Bytes("Mvp"), Txn.sender()),
+            App.globalPut(Bytes("MaxDamage"), playerTotalDamage)
+        ])
+    )
+
+    attack_monster = Seq([
+        Assert(currentMonsterHealth > Int(0)),
+        update_mvp,
+        update_player_total_damage,
+        update_monster_health,
+        Return(Int(1))
+    ])
+
+    '''
+    Reward player
+    '''
+    mvp = App.globalGet(Bytes("Mvp"))
+    reward_player = Seq([
+        Assert(currentMonsterHealth <= Int(0)),
+        Assert(Txn.accounts[1] == mvp),
+        InnerTxnBuilder.Begin(),
+        InnerTxnBuilder.SetFields({
+            TxnField.type_enum: TxnType.Payment,
+            TxnField.receiver: Txn.accounts[1],
+            TxnField.amount: Int(1000000),
+        }),
+        InnerTxnBuilder.Submit(),
+        Return(Int(1))
+    ])
+
+    handle_noop = Seq(
+        Assert(Global.group_size() == Int(1)), 
+        Cond(
+            [Txn.application_args[0] == Bytes("Attack"), attack_monster],
+            [Txn.application_args[0] == Bytes("Reward"), reward_player]
+        )
+    )
+
     handle_closeout = Return(Int(1))
     handle_updateapp = Return(Int(0))
     handle_deleteapp = Return(Int(1))
--- scripts/actions/attack.js
+++ scripts/actions/attack.js
@@ -2,7 +2,33 @@ const { executeTransaction, convert, readAppGlobalState, readAppLocalState } = r
 const { types } = require("@algo-builder/web");
 
 async function run(runtimeEnv, deployer) {
-    // write your code here
+    const master = deployer.accountsByName.get("master");
+    const acc1 = deployer.accountsByName.get("acc1");
+    const approvalFile = "game_approval.py";
+    const clearStateFile = "game_clearstate.py";
+
+    // get app info
+    const gameApp = deployer.getApp(approvalFile, clearStateFile);
+    const appID = gameApp.appID;
+    let globalState = await readAppGlobalState(deployer, master.addr, appID);
+    console.log(globalState);
+
+    // commence attack
+    const attackAppArgs = ["Attack"].map(convert.stringToBytes);
+    await executeTransaction(deployer, {
+        type: types.TransactionType.CallApp,
+        sign: types.SignType.SecretKey,
+        fromAccount: acc1,
+        appID: appID,
+        payFlags: { totalFee: 1000 },
+        appArgs: attackAppArgs,
+    });
+
+    // get global and local state
+    globalState = await readAppGlobalState(deployer, master.addr, appID);
+    console.log(globalState);
+    playerState = await readAppLocalState(deployer, acc1.addr, appID);
+    console.log(playerState);
 }
 
 module.exports = { default: run };
